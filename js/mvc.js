cc53c5601765f8c344606add58896f7389f5e35e
unknown989 <55900066+unknown989@users.noreply.github.com>
2022-03-14 22:44:17 +0100

    Update mvc.js

--- js/mvc.js
+++ js/mvc.js
@@ -1,41 +1,48 @@
-const HOSTPATH = "/Petite-MVC";
-class MVC{
-    constructor(){
-        this.routes = [];
-        const alinks = document.getElementsByClassName("link");
-        [...alinks].forEach((alink)=>{
-            alink.addEventListener("click",(e)=>{
-                e.preventDefault();
-                const _route = this.routes.find((r)=>r.to === alink.getAttribute("to"));
-                this.changeURL(_route.to);
-            })
-        })
-        window.onpopstate = (e)=>{
-            const root = document.getElementById("root");
-            const _route = this.getRouteByPathname(window.location.pathname);
-            root.innerHTML = _route.path();
-        };
-        window.onload = ()=>{dispatchEvent(new Event("popstate"))}
-        
+var hostpathname = "";
+{
+  var tmp = window.location.pathname.split("/");
+  hostpathname = tmp.slice(-1);
+  if (hostpathname == "") {
+    hostpathname = tmp.slice(-1);
+  }
+}
+class MVC {
+  constructor() {
+    this.routes = [];
+    const alinks = document.getElementsByClassName("link");
+    [...alinks].forEach((alink) => {
+      alink.addEventListener("click", (e) => {
+        e.preventDefault();
+        const _route = this.routes.find(
+          (r) => r.to === alink.getAttribute("to")
+        );
+        this.changeURL(_route.to);
+      });
+    });
+    window.onpopstate = (e) => {
+      const root = document.getElementById("root");
+      const _route = this.getRouteByPathname(window.location.pathname);
+      root.innerHTML = _route.path();
     };
-    addRoute(name,to,path){
-        this.routes.push({name:name,to:to,path:path});
+    window.onload = () => {
+      dispatchEvent(new Event("popstate"));
     };
-    changeURL(pathname){
-        var path = pathname;
-        if(path[0] == "/"){
-            path = HOSTPATH+path
-        }else{
-            path = HOSTPATH+"/"+path;
-        }
-        window.history.pushState({},'',path);
-        dispatchEvent(new Event("popstate"))
-    };
-    getRouteByPathname(to){
-        if(to){
-            const _r = this.routes.find((r)=>r.to === to);
-            return _r;
-        }
-    };
+  }
+  addRoute(name, to, path) {
+    this.routes.push({ name: name, to: to, path: path });
+  }
+  changeURL(pathname) {
+    var path = pathname;
 
+    path = `${hostpathname}${path}`;
+    console.log(path);
+    window.history.pushState({}, "", path);
+    dispatchEvent(new Event("popstate"));
+  }
+  getRouteByPathname(to) {
+    if (to) {
+      const _r = this.routes.find((r) => r.to === to);
+      return _r;
+    }
+  }
 }
